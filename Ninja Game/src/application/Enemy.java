7d516aa4085ba1c7181f35709293a721e5bf5ead
nic <nic@172.20.10.7>
2017-06-03 18:46:27 -0400

    New Enemy Class

---
+++ Ninja Game/src/application/Enemy.java
@@ -0,0 +1,16 @@
+package application;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Enemy extends GameObject{
+	
+	private Image[] state;
+	private boolean alive;
+
+	public Enemy(ImageView view) {
+		super(view);
+		// TODO Auto-generated constructor stub
+	}
+
+}
